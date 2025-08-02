df890323f9fa07deff08a9a919fce9b8348ff30b
Ej Cooper <ejcooper@Ejs-MacBook-Pro.local>
2025-08-02 00:44:25 -0400

    Fix: Update email sending API

--- src/lib/google-auth.ts
+++ src/lib/google-auth.ts
@@ -1,11 +1,10 @@
 import { google } from 'googleapis';
 
-const auth = new google.auth.GoogleAuth({
-  credentials: {
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  },
-  scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'],
+export const auth = new google.auth.JWT({
+  email: process.env.GOOGLE_CLIENT_EMAIL!,
+  key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+  scopes: [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive',
+  ],
 });
-
-export { auth };
